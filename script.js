e716c5674afa2aff970c6fefea6839f99d9de44d
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-01-24 08:08:11 +0200

    updated script

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const revealElements = document.querySelectorAll("[data-reveal]");
+const revealElements = document.querySelectorAll("data-reveal");
 
-const scrollReaveal = function () {
-    for(let i = 0, len = revealElements.length; i < len; i++) {
-            const isElementOnScreen = revealElements[i].
-            getBoundingClientRect().top < window.innerHeight;
-        
-        if (isElementOnScreen) {
-          revealElements[i].classList.add("revealed")
-        }else {
-            revealElements[i].classList.remove("revealed");
-        }
-     }
+const scrollReaveal = function (){
+
+  for(let i = 0, len =revealElements.length; i < len; i++) {
+    const isElementsOnScreen = revealElements[i].getBouncingClientRect().top < window.innerHeight;
+    if(isElementsOnScreen){
+      revealElements[i].classList.add("revealed")
+    }else {
+      revealElements[i].classList.remove("revealed")
     }
+  }
+}
 
-    window.addEventListener("scroll", scrollReaveal);
+window.addEventListener("scroll", scrollReaveal);
